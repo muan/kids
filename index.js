998e30e1ea07e689e952ab71134d5e06cc822470
Mu-An Chiou <me@muanchiou.com>
2023-02-16 14:24:31 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('form')
+const fromInput = document.querySelector('[data-from-date]')
+const events = document.querySelectorAll('[data-event]')
+
+form.addEventListener('reset', filter)
+form.addEventListener('change', filter)
+
+function filter(dateTime) {
+    if (!fromInput.value || !dateTime) return
+    const fromDate = dateTime || new Date(fromInput.value)
+    if (!fromDate) return
+
+    for (const event of events) {
+        const eventDate = new Date(event.getAttribute('data-datetime'))
+        event.hidden = eventDate <= fromDate
+    }
+}
+
+function showAll() {
+    for (const event of events) {
+        event.hidden = false
+    }
+}
+
+const fromDateString = new URLSearchParams(location.search).get('from')
+const date = new Date(fromDateString)
+if (fromDateString && date) {
+    fromInput.value = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
+    filter(date)
+}
