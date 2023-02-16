749f501dd57bf5beb70f6f6cd243e5ce3f47e272
Mu-An Chiou <me@muanchiou.com>
2023-02-16 14:31:37 +0800

    Fix js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const events = document.querySelectorAll('[data-event]')
 form.addEventListener('reset', filter)
 form.addEventListener('change', filter)
 
-function filter(dateTime) {
-    if (!fromInput.value && !dateTime) return
-    const fromDate = dateTime || new Date(fromInput.value)
-    if (!fromDate) return
+function filter() {
+    if (!fromInput.value) { showAll(); return }
+    const fromDate = new Date(fromInput.value)
+    if (!fromDate) { showAll(); return }
 
     for (const event of events) {
         const eventDate = new Date(event.getAttribute('data-datetime'))
@@ -26,5 +26,5 @@ const fromDateString = new URLSearchParams(location.search).get('from')
 const date = new Date(fromDateString)
 if (fromDateString && date) {
     fromInput.value = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
-    filter(date)
+    filter()
 }
